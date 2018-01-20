a1edaaa33d1525812e024780bd3967f6177046b4
Daniele Bonacorsi <bonacorsi@bo.infn.it>
2018-01-20 17:34:37 +0100

    Add argparse and output in a csv file

--- exampleMacro.py
+++ exampleMacro.py
@@ -1,21 +1,53 @@
 import ROOT as root
 import numpy as np
-
+import csv
+import argparse
 #root.gInterpreter.ProcessLine('#include "interface/MuonPogTree.h"')
 #root.gSystem.Load('MuonPogTreeDict_rdict')
-root.gROOT.LoadMacro('interface/MuonPogTree.h++')
 
+root.gROOT.LoadMacro('interface/MuonPogTree.h++')
 from ROOT.muon_pog import Event
 
-inputFile   = root.TFile("/afs/cern.ch/user/b/battilan/public/trigger_and_machine_learning/MuonTree.root")
-muonPogTree = inputFile.Get("MuonPogTree/MUONPOGTREE")
+class OptionParser():
+    def __init__(self):
+        "User based option parser"
+        self.parser = argparse.ArgumentParser(prog='PROG')
+        self.parser.add_argument("--fin", action="store",
+            dest="inputFile", default="", help="Input ROOT file")
+        self.parser.add_argument("--branch", action="store",
+            dest="branch", default="events", help="Input ROOT file branch (default events)")
+        self.parser.add_argument("--branches", action="store",
+            dest="branches", default="", help="ROOT branches to read, 'Electron_,Jet_'")
+        self.parser.add_argument("--fout", action="store",
+            dest="fout", default="output.csv", help="Output CSV file")
 
-print muonPogTree.Print()
+def main():
+    "Main function"
+    optmgr  = OptionParser()
+    opts = optmgr.parser.parse_args() 
+    opts.inputFile = root.TFile("/afs/cern.ch/work/b/bonacor/TOMMASO/MuonTree.root")
+    opts.branch = opts.inputFile.Get("MuonPogTree/MUONPOGTREE")
+    branches = [b.strip() for b in opts.branches.split(',')]
+    new_branch = []
+    print branches
+    #print opts.branch.Print()
 
-entries = muonPogTree.GetEntriesFast()
+    entries = opts.branch.GetEntriesFast()
 
-for entry in muonPogTree :
-    for dtPrimitive in entry.event.dtPrimitives :
-        print entry.event.eventNumber, dtPrimitive.id_eta, dtPrimitive.id_phi, \
-              dtPrimitive.id_r, dtPrimitive.bxTrackFinder(), dtPrimitive.phiGlb()
+    with open(opts.fout,'wb') as csvfile:
+        writer = csv.writer(csvfile)
+        for entry in opts.branch:
+            for dtPrimitive, genParticle in [(dtPrimitive,genParticle) for dtPrimitive in entry.event.dtPrimitives for genParticle in entry.event.genParticles]:
+                for branch in branches:
+                    new_branch.append(eval(branch))
+                if dtPrimitive.bx == 0:
+                #writer.writerow(entry.event.eventNumber, dtPrimitive.id_r, dtPrimitive.id_phi, \
+                #dtPrimitive.id_eta, dtPrimitive.bxTrackFinder(), dtPrimitive.phiGlb(), genParticle.pt
+                    #branches.insert(0,entry.event.eventNumber)
+                    new_branch.insert(0,entry.event.eventNumber)        
+                    writer.writerow(new_branch)
+                    del new_branch[:] 
+
+if __name__ == '__main__':
+    main()
 
