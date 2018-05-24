40ee228191126a04f78717624b71951779b5966c
Tommaso <tommaso.diotalevi@gmail.com>
2018-05-24 19:30:36 +0200

    Add changes

--- analysisMacro.py
+++ analysisMacro.py
@@ -59,44 +59,6 @@ for entry in muonPogTree :
         minDr = 999.
         bestL1MuonVector = root.TLorentzVector()
         bestL1MuonCharge = 0
- 
-        for dtPrimitive in entry.event.dtPrimitives:
-            i=1
-            name = i + 'dtPrimitive.id_r' 
-            input_dataframe[name] = dtPrimitive.id_r
-        for dtPrimitive in entry.event.dtPrimitives:
-            i=1
-            name = i + 'dtPrimitive.id_eta' 
-            input_dataframe[name] = dtPrimitive.id_eta
-        for dtPrimitive in entry.event.dtPrimitives:
-            i=1
-            name = i + 'dtPrimitive.id_phi' 
-            input_dataframe[name] = dtPrimitive.id_phi
-        for dtPrimitive in entry.event.dtPrimitives:
-            i=1
-            name = i + 'dtPrimitive.id_phiB' 
-            input_dataframe[name] = dtPrimitive.id_phiB
-        for dtPrimitive in entry.event.dtPrimitives:
-            i=1
-            name = i + 'dtPrimitive.id_quality' 
-            input_dataframe[name] = dtPrimitive.id_quality
-        for dtPrimitive in entry.event.dtPrimitives:
-            i=1
-            name = i + 'dtPrimitive.phiGlb' 
-            input_dataframe[name] = dtPrimitive.phiGlb()
-        
-        cols = input_dataframe.columns.values.tolist()
-        input_dataframe.insert(len(cols)-1,"delta_phi34", abs(input_dataframe["3dtPrimitive.phiGlb"]-input_dataframe["4dtPrimitive.phiGlb"]))
-        input_dataframe.insert(len(cols)-1,"delta_phi24", abs(input_dataframe["2dtPrimitive.phiGlb"]-input_dataframe["4dtPrimitive.phiGlb"]))
-        input_dataframe.insert(len(cols)-1,"delta_phi23", abs(input_dataframe["2dtPrimitive.phiGlb"]-input_dataframe["3dtPrimitive.phiGlb"]))
-        input_dataframe.insert(len(cols)-1,"delta_phi14", abs(input_dataframe["1dtPrimitive.phiGlb"]-input_dataframe["4dtPrimitive.phiGlb"]))
-        input_dataframe.insert(len(cols)-1,"delta_phi13", abs(input_dataframe["1dtPrimitive.phiGlb"]-input_dataframe["3dtPrimitive.phiGlb"]))
-        input_dataframe.insert(len(cols)-1,"delta_phi12", abs(input_dataframe["1dtPrimitive.phiGlb"]-input_dataframe["2dtPrimitive.phiGlb"]))
-        
-        for column in df.columns.values.tolist():
-            if "phiGlb" in column:
-                df = df.drop(column,axis=1)
-        print(input_dataframe)
         
         for l1Muon in entry.event.l1muons :
 
@@ -120,7 +82,7 @@ for entry in muonPogTree :
     
         # We are only interested in pT assignment
         # therefore we require the dR matching to be < 0.3 already
-        input_dataframe.iloc[0:0]
+
         if minDr < 0.3 :
             histos["L1_SingleMuon22_Eff_Vs_Pt"].Fill(bestL1MuonVector.Pt() >= 22, \
                                                      genMuonVector.Pt())
